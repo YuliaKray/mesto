f2aba6521d5be42ce32073496b0f02b89c32872d
Yulia Kraynova <ungo1998s@yandex.ru>
2023-03-24 14:34:21 +0300

    добавлен файл JS

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const editButton = document.querySelector('.profile__edit-button');
 const editButtonPopup = document.querySelector('.popup_type_edit');
 const editPopupClose = editButtonPopup.querySelector('.popup__close');
+const editPopupName = editButtonPopup.querySelector('.popup__form-text_type_name');
+const editPopupDescription = editButtonPopup.querySelector('.popup__form-text_type_description');
+const editPopupSubmit = editButtonPopup.querySelector('.popup__submit-button');
+const formElement = editButtonPopup.querySelector('.popup__form');
+const nameInput = document.querySelector('.profile__name');
+const jodInput = document.querySelector('.profile__caption');
 
-console.log(editPopupClose);
 
-function editButtonClick(){
+console.log(formElement);
+
+editButton.addEventListener('click', function(){
   editButtonPopup.classList.add('popup_open');
-}
+  editPopupName.value = nameInput.textContent;
+  editPopupDescription.value = jodInput.textContent;
+});
 
-editButton.addEventListener('click', editButtonClick);
+editPopupClose.addEventListener('click', function(){
+  editButtonPopup.classList.remove('popup_open');
+});
 
-function editButtonClickClose(){
+function handleFormSubmit(event){
+  event.preventDefault();
+
+  nameInput.textContent = editPopupName.value;
+  jodInput.textContent = editPopupDescription.value;
   editButtonPopup.classList.remove('popup_open');
 }
 
-editPopupClose.addEventListener('click', editButtonClickClose);
-
+editPopupSubmit.addEventListener('click', handleFormSubmit);
